22ac38c1be6a091d7c7dbf58d0e4bfcb92d93a5f
fabioleite2024 <fabioleite2024@gmail.com>
2023-10-06 13:49:20 -0300

    up

---
+++ assets/php/salva_dados.php
@@ -0,0 +1,34 @@
+<?php
+// Conexão com o banco de dados MySQL
+$servername = "https://website2425.000webhostapp.com/";
+$username = "id17772163_new";
+$password = "114811t@F@";
+$dbname = "id17772163_new";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifique a conexão
+if ($conn->connect_error) {
+    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+}
+
+// Receba os dados do formulário
+$nome = $_POST['name'];
+$email = $_POST['email'];
+$mensagem = $_POST['message'];
+
+// Prepare e execute a consulta SQL para inserir os dados
+$sql = "INSERT INTO mensagens (nome, email, mensagem) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $nome, $email, $mensagem);
+
+if ($stmt->execute()) {
+    echo "Mensagem enviada com sucesso!";
+} else {
+    echo "Erro ao enviar a mensagem: " . $stmt->error;
+}
+
+// Feche a conexão com o banco de dados
+$stmt->close();
+$conn->close();
+?>
