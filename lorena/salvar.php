a1e2fd9b1bc4ac91a7c1317c2040b993c8102440
fabioleite2024 <112669819+fabioleite2024@users.noreply.github.com>
2026-06-08 06:06:56 -0300

    Update salvar.php

--- lorena/salvar.php
+++ lorena/salvar.php
@@ -1,48 +1,69 @@
 <?php
 
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
 
-$arquivo = "data/rifa.json";
-
-$dados = json_decode(
-    file_get_contents("php://input"),
-    true
-);
-
-$lista = [];
-
-if(file_exists($arquivo))
+try
 {
+    $arquivo = __DIR__ . '/data/rifa.json';
+
+    $dados = json_decode(
+        file_get_contents("php://input"),
+        true
+    );
+
+    if(!$dados)
+    {
+        throw new Exception("Dados inválidos.");
+    }
+
+    if(!file_exists($arquivo))
+    {
+        file_put_contents($arquivo, "[]");
+    }
+
     $lista = json_decode(
         file_get_contents($arquivo),
         true
     );
-}
 
-foreach($lista as $item)
-{
-    if($item["numero"] == $dados["numero"])
+    if(!$lista)
     {
-        echo json_encode([
-            "sucesso"=>false,
-            "mensagem"=>"Número já vendido."
-        ]);
-        exit;
+        $lista = [];
     }
+
+    foreach($lista as $item)
+    {
+        if($item["numero"] == $dados["numero"])
+        {
+            echo json_encode([
+                "sucesso"=>false,
+                "mensagem"=>"Número já reservado."
+            ]);
+
+            exit;
+        }
+    }
+
+    $lista[] = $dados;
+
+    file_put_contents(
+        $arquivo,
+        json_encode(
+            $lista,
+            JSON_PRETTY_PRINT |
+            JSON_UNESCAPED_UNICODE
+        )
+    );
+
+    echo json_encode([
+        "sucesso"=>true,
+        "mensagem"=>"Reserva realizada com sucesso!"
+    ]);
 }
-
-$lista[] = $dados;
-
-file_put_contents(
-    $arquivo,
-    json_encode(
-        $lista,
-        JSON_PRETTY_PRINT |
-        JSON_UNESCAPED_UNICODE
-    )
-);
-
-echo json_encode([
-    "sucesso"=>true,
-    "mensagem"=>"Número reservado com sucesso!"
-]);
+catch(Exception $e)
+{
+    echo json_encode([
+        "sucesso"=>false,
+        "mensagem"=>$e->getMessage()
+    ]);
+}
